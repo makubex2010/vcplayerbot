9d0b971d35bfc2d80a7827534bd733ebaf18c346
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-11-15 22:06:11 +0800

    Update shutdown.py

--- extras/shutdown.py
+++ extras/shutdown.py
@@ -4,7 +4,7 @@ from utils import logger, logInfo
 
 @logger.catch
 async def shutdown(signal, loop):
-    logInfo(f"Received exit signal {signal.name}...")
+    logInfo(f"收到退出信號 {signal.name}...")
 
     # close the music player instances
     # mp = MusicPlayer()
@@ -14,6 +14,6 @@ async def shutdown(signal, loop):
 
     [task.cancel() for task in tasks]
 
-    logInfo(f"Cancelling {len(tasks)} outstanding tasks")
+    logInfo(f"取消 {len(tasks)} 未完成的任務")
     await asyncio.gather(*tasks, return_exceptions=True)
     loop.stop()
