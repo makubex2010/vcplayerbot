92d547c6966e112a88967b8507a1f87871e15348
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-11-15 21:31:46 +0800

    Update generateSession.py

--- generateSession.py
+++ generateSession.py
@@ -5,10 +5,10 @@ from pyrogram import Client
 
 try:
     print(
-        "確保您有 API ID 和HASH，如果沒有，請轉到 my.telegram.org 並生成它。\n\n"
+        "請確認您有 API ID 和API HASH，如果沒有，請到 my.telegram.org 並申請。\n\n"
     )
-    API_ID = input("Enter Your API ID -  ")
-    API_HASH = input("Enter Your API HASH -  ")
+    API_ID = input("輸入您的 API ID -")
+    API_HASH = input("輸入您的 API HASH -")
 
     print(
         "\n\n現在它會要求您輸入您的電話號碼（國際格式），然後按照步驟操作"
@@ -19,7 +19,7 @@ try:
     with client:
         session = client.export_session_string()
         print(
-            "\n完成您的會話字符串將保存在您保存的消息中！ 不要與其他任何人分享。"
+            "\n完成!您的會話字符串將保存在您保存的消息中！ 不要與其他任何人分享。"
         )
         client.send_message(
             "me",
