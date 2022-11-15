d6c65e215bee32796a9a92130e15ad3c1279b53a
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-11-15 21:27:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
             or (not config.get("USERBOT_SESSION") and not config.get("MONGO_URL"))
         ):
             logWarning(
-                f"Please provide the required values in .env file, Please check {config.get('SESSION_STRING_STEPS')} on how to get the values"
+                f"請在 .env 文件中提供所需的值，請檢查 {config.get('SESSION_STRING_STEPS')} 如何獲取這些值"
             )
             sys.exit(0)
         # create the images and song folder if not there
@@ -54,7 +54,7 @@ def main():
         )
 
         def sig_handler(signum, frame):
-            logException(f"Segmentation Fault : {signum} : {frame}", True)
+            logException(f"分段故障 ： {signum} : {frame}", True)
 
         signal.signal(signal.SIGSEGV, sig_handler)
 
@@ -65,7 +65,7 @@ def main():
                     s, lambda s=s: asyncio.create_task(shutdown(s, loop))
                 )
         except NotImplementedError:
-            logWarning("Not implemented on windows device. Can safely ignore.")
+            logWarning("未在 Windows 設備上實現。 可以放心忽略.")
 
         bot.start()
 
@@ -78,11 +78,11 @@ def main():
 
         loop.run_forever()
     except KeyboardInterrupt as k:
-        logWarning("Client Keyboard Interrupt, Exiting Service.")
+        logWarning("客戶端鍵盤中斷，退出服務。")
     except Exception as ex:
-        logException(f"Error in main method : {ex}", True)
+        logException(f"主要方法錯誤：{ex}", True)
     finally:
-        logException(f"Closed the service :)", True)
+        logException(f"關閉服務：)", True)
         if bot is not None and bot.is_connected:
             bot.stop()
 
@@ -90,11 +90,11 @@ def main():
 if __name__ == "__main__":
     try:
         logInfo(
-            "Started the Backend Server For Voice Chat Music Player | A SkTechHub Product"
+            "啟動語音聊天音樂播放器的後端服務器 | SkTechHub 產品"
         )
         main()
     except Exception as ex:
-        logException(f"Error in main start : {ex}", True)
+        logException(f"主啟動錯誤： {ex}", True)
     finally:
-        logInfo("Closing the service :)")
+        logInfo("關閉服務：)")
         sys.exit(0)
